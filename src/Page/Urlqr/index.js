ce859607f422ceb394c3069f5de619703eaa5ed8
webcolmeia <eulukasthyago@protonmail.com>
2020-09-01 16:09:14 +0000

    update page url and form qr code

--- src/Page/Urlqr/index.js
+++ src/Page/Urlqr/index.js
@@ -1,30 +1,81 @@
-import React, { useState } from 'react';
-import { Container } from '../../Components/Contaier';
-import { Main } from '../../Components/Main';
-import { PageContainer } from './style';
+import React, { useState } from "react";
+import { Container } from "../../Components/Contaier";
+import { Main } from "../../Components/Main";
+import { PageContainer, Input, Select } from "./style";
 import QRCode from "react-qr-code";
 
 const Urlqr = () => {
+  const [urlValue, setUrlValue] = useState("");
+  const [sizeValue, setSizeValue] = useState(128);
+  const [fgValue, setFgValue] = useState("#000000");
+  const [bgValue, setBgValue] = useState("#FFFFFF");
+  const [lavelValue, setLavelValue] = useState("L");
 
-    const [textValue, setTextValue] = useState("");
-
-    return (
-        <Container>
-            <Main>
-                <h1>
-                    Texto
-                </h1>
-                <PageContainer>
-                    <div>
-                         <input type="text" value={textValue} onChange={text => setTextValue(text.target.value)} />
-                    </div>
-                    <div>
-                        <QRCode value={textValue} />
-                    </div>
-                </PageContainer>
-            </Main>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Main>
+        <h1>Gerar Qr Code de urls</h1>
+        <PageContainer>
+          <div>
+            <Input>
+              <Input.Title>Url:</Input.Title>
+              <Input.Field
+                type="text"
+                value={urlValue}
+                onChange={(text) => setUrlValue(text.target.value)}
+              />
+            </Input>
+            <Input>
+              <Input.Title>Tamanho do qr code:</Input.Title>
+              <Input.Field
+                type="number"
+                onChange={(text) => setSizeValue(text.target.value)}
+              />
+            </Input>
+            <Input>
+              <Input.Title>Cor primaria:</Input.Title>
+              <Input.Field
+                value={fgValue}
+                type="color"
+                onChange={(text) => setFgValue(text.target.value)}
+              />
+            </Input>
+            <Input>
+              <Input.Title>Cor do fundo:</Input.Title>
+              <Input.Field
+                value={bgValue}
+                type="color"
+                onChange={(text) => setBgValue(text.target.value)}
+              />
+            </Input>
+            <Select>
+              <Select.Title>Level:</Select.Title>
+              <Select.Field
+                value={lavelValue}
+                onChange={(text) => setLavelValue(text.target.value)}
+              >
+                <option selected value="L">
+                  L
+                </option>
+                <option value="M">M</option>
+                <option value="Q">Q</option>
+                <option value="H">H</option>
+              </Select.Field>
+            </Select>
+          </div>
+          <div>
+            <QRCode
+              value={urlValue}
+              size={sizeValue}
+              bgColor={bgValue}
+              fgColor={fgValue}
+              level={lavelValue}
+            />
+          </div>
+        </PageContainer>
+      </Main>
+    </Container>
+  );
+};
 
 export default Urlqr;
--- src/Page/Urlqr/style.js
+++ src/Page/Urlqr/style.js
@@ -12,3 +12,43 @@ export const PageContainer = styled.div`
         }
     }
 `;
+
+export const Input = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    margin: 15px 0;
+`;
+
+Input.Field = styled.input`
+    padding: 10px 15px;
+    border: 1px solid #999;
+    border-radius: 3px;
+`;
+
+Input.Title = styled.label`
+    font-size: 18px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+`;
+
+export const Select = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    margin: 15px 0;
+`;
+
+Select.Title = styled.label`
+    font-size: 18px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+`;
+
+Select.Field = styled.select`
+    padding: 10px 15px;
+    border: 1px solid #999;
+    border-radius: 3px;
+`;
